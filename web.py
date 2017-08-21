dcc0d8087d4ce98f01da43b895b2d7919d4d1f6d
abiniyam <abiniyam@gmail.com>
2017-08-21 15:59:51 +0300

    -

--- web.py
+++ web.py
@@ -30,5 +30,14 @@ def send_message(text, chat_id):
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
     
-text, chat = get_last_chat_id_and_text(get_updates())
-send_message(text, chat)
+def main():
+    last_textchat = (None, None)
+    while True:
+        text, chat = get_last_chat_id_and_text(get_updates())
+        if (text, chat) != last_textchat:
+            send_message(text, chat)
+            last_textchat = (text, chat)
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    main()
