23b18205f2d03c70c0193be4dd5fa20962947896
abiniyam <abiniyam@gmail.com>
2017-08-23 12:25:42 +0300

    Update web.py

--- web.py
+++ web.py
@@ -1,6 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
+from telegram.ext import Updater, CommandHandler
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+token = "306226834:AAG23HIKPbvhSKsFpMyIREdfUOnArEDUwRs"
+URL = "https://api.telegram.org/bot{}/".format(token)
+PORT = int(os.environ.get('PORT', '5000'))
+
+
+
+
+def start(bot, update):
+    update.message.reply_text('Hello World!')
+
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name)+" Test")
+
+updater = Updater(token)
+
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.start_polling()
+updater.idle()
